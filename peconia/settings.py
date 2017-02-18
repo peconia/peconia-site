deea91de31352e71aec3d8aebd9a09a2d74d000d
Paivi Suomela <paivi.e.suomela@gmail.com>
2017-02-18 15:29:26 +0000

    removed postgres stuff

--- peconia/settings.py
+++ peconia/settings.py
@@ -125,9 +125,6 @@ STATICFILES_DIRS = [
     '/var/www/static/',
 ]
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
